1f7714a73671390ff503ee58f3632795ef313bc3
unknown <ankur.onlymee@gmail.com>
2024-06-11 12:41:57 +0530

    update file pv

--- teraa@tmp/durable-9b8497fc/jenkins-wrap.bat
+++
@@ -1,3 +0,0 @@
-@echo off 
-cmd /c call "C:/Users/pc/DEVOPS Assignments/capstone\teraa@tmp\durable-9b8497fc\jenkins-main.bat" > "C:/Users/pc/DEVOPS Assignments/capstone\teraa@tmp\durable-9b8497fc\jenkins-log.txt" 2>&1
-echo %ERRORLEVEL% > "C:/Users/pc/DEVOPS Assignments/capstone\teraa@tmp\durable-9b8497fc\jenkins-result.txt"
