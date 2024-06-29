28209d9a5389290a057ddc49678f4f72befcb24b
unknown <ankur.onlymee@gmail.com>
2024-06-29 12:14:28 +0530

    increse node

--- teraa/main.tf
+++ teraa/main.tf
@@ -133,9 +133,9 @@ resource "aws_eks_node_group" "node_group" {
   subnet_ids      = aws_subnet.public[*].id
 
   scaling_config {
-    desired_size = 3
-    max_size     = 5
-    min_size     = 3
+    desired_size = 4
+    max_size     = 7
+    min_size     = 4
   }
 
   instance_types = ["t3a.large"]
